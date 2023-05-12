084ff8dc576f01849b3e8c108f609db0a311bddf
Rodrigo Burgos <76675806+burgossrodrigo@users.noreply.github.com>
2023-05-11 21:22:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require('cors')
 const bodyParse = require('body-parser')
 const app = express();
 
+const { tokenCachedMiddleware, sixHourCandleCachedMiddleware, oneHourCandleCachedMiddleware } = require("./cache");
+const { tokensController, sixHourCandleController, oneHourCandleController } = require("./controller");
+
 const corsOptions = {
   origin: [
     'http://localhost:3000',
@@ -16,14 +19,13 @@ const router = express.Router()
 app.use(cors());
 app.use(json());
 app.use(router);
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server is listening on port: ${PORT}`)
 })
 
-const { tokenCachedMiddleware, sixHourCandleCachedMiddleware, oneHourCandleCachedMiddleware } = require("./cache");
-const { tokensController, sixHourCandleController, oneHourCandleController } = require("./controller");
+
 
 
 // var server_host = process.env.YOUR_HOST || '0.0.0.0';
